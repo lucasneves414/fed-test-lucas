ca1c0b580eb3b0aa6881da6a3016e0a0e974d6cc
LUCAS NEVES <42455120+lucasneves414@users.noreply.github.com>
2022-08-12 09:57:24 -0300

    Fed Test

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
@@ -16,11 +16,14 @@ var reportsWidget = {
                     '</footer>' +
                 '</article>' +
             '{{/.}}'
-        )
+        ),
+        current_page: 1,
+        obj_per_page: 9,
     },
+    page: 0,
 
     init: function() {
-        this.renderReports(reportData || []);
+        this.renderReports(reportData.slice(0, 9) || []);
     },
 
     renderReports: function(reports) {
@@ -28,6 +31,33 @@ var reportsWidget = {
             options = inst.options;
 
         $(options.containerSelector).html(Mustache.render(options.template, reports));
+    },
+
+    next: function(nextArrays) {
+        reportsWidget.page = nextArrays <= 9 ? nextArrays + 9 : nextArrays;
+        console.log('aaa', reportData.slice(reportsWidget.page - 9, reportsWidget.page));
+        if(reportData.slice(reportsWidget.page - 9, reportsWidget.page) && reportData.slice(reportsWidget.page - 9, reportsWidget.page).length > 0){
+            reportsWidget.renderReports(reportData.slice(reportsWidget.page - 9, reportsWidget.page));
+        } else {
+            reportsWidget.page = 0;
+            this.renderReports(reportData.slice(0, 9) || []);
+        }
+        
+    },
+
+    previous: function(nextArrays) {
+        reportsWidget.page = nextArrays;
+        if(reportsWidget.page <= 0){
+            reportsWidget.page = 9;
+            return;
+        }
+        console.log('aaa', reportData.slice(reportsWidget.page - 9, reportsWidget.page));
+        if(reportData.slice(reportsWidget.page - 9, reportsWidget.page) && reportData.slice(reportsWidget.page - 9, reportsWidget.page).length > 0){
+            reportsWidget.renderReports(reportData.slice(reportsWidget.page - 9, reportsWidget.page));
+        } else {
+            return;
+        }
+        
     }
 };
 
